cdfafde44b027aeb859619289775ad27d3322755
ghiat <ghiat@DESKTOP-211S7BF>
2016-04-27 19:10:17 +0100

    testing changes in registers.

---
+++ examples/testing_ex.rs
@@ -0,0 +1,19 @@
+extern crate frounding;
+
+use frounding::RoundingState;
+
+// I used this example to make sure that the changes occur at the registers, using the gdb.
+
+fn main() {
+
+    let mut rs = RoundingState::new();
+
+    rs.upward();
+
+    rs.downward();
+
+    rs.to_nearest();
+
+    rs.truncate();
+
+}
